e344732ef741425919488dde1e1d541d50b3120e
Rita Tang <doggymint@gmail.com>
2023-08-30 13:22:39 -0400

    prompt the user and ask for input before overwriting an existing file

--- fetcher.js
+++ fetcher.js
@@ -8,16 +8,35 @@ const fetcher = (url, filePath) => {
       console.log(error);
       process.exit();
     }
+    // Try to read if the file exists
+    fs.readFile(filePath, (err, data) => {
+      // if data is undefined, it means there's no such file
+      if (data) {
+        console.log("File already exists, overwrite it? (y/n)");
+        const stdin = process.stdin;
+        stdin.setRawMode(true);
+        stdin.setEncoding("utf8");
 
-    fs.writeFile(filePath, body, (err) => {
-      // exit the writeFile process when there is an error in writing the file
-      if (err) {
-        console.error(err);
-        process.exit();
+        //listen for key press
+        stdin.on("data", (key) => {
+          if (key === "\u0003") {
+            process.exit();
+          } else if (key === "N" || key === "n") {
+            console.log("The file is not replaced, process ended.");
+            process.stdin.pause();
+          } else if (key === "Y" || key === "y") {
+            console.log("File will be replaced.");
+            process.stdin.pause();
+            writeFile(filePath, body);
+          } else {
+            // when the input is neither n nor y, continue listen for keypress
+            console.log("Invalid input:", key);
+            process.stdin.resume();
+          }
+        });
+      } else {
+        writeFile(filePath, body);
       }
-      // file written successfully
-      // Output format: "Downloaded and saved 1235 bytes to ./index.html."
-      console.log(`Downloaded and saved ${body.length} bytes to ${filePath}.`);
     });
   });
 };
@@ -30,6 +49,20 @@ const argValidator = (args, fetchFunction) => {
   }
   const [url, filePath] = args;
   return fetchFunction(url, filePath);
+};
+
+// writeFile function takes the body downloaded from the URL and write to the filePath
+const writeFile = (filePath, body) => {
+  fs.writeFile(filePath, body, (err) => {
+    // exit the writeFile process when there is an error in writing the file
+    if (err) {
+      console.error(err);
+      process.exit();
+    }
+    // file written successfully
+    // Output format: "Downloaded and saved 1235 bytes to ./index.html."
+    console.log(`Downloaded and saved ${body.length} bytes to ${filePath}.`);
+  });
 };
 
 const args = process.argv.slice(2);
